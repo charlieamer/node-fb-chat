60dc7f4e5f731fb02f3469c5fcc4b1eae217863d
harisb <haris.botic96@gmail.com>
2014-09-09 23:09:41 +0200

    test new message

--- LittleChatApp/routes/chat.js
+++ LittleChatApp/routes/chat.js
@@ -3,10 +3,13 @@
  * GET last 10 messages in room
  */
 
-var waiting_list ={};
+var waiting_list = {};
+
+var slow_waiting_list = [];
 
 exports.new_message = function (req, res) {
-    res.json(['nova poruka, obiscni json']);
+    res.json(JSON.parse(req.body));
+
 };
 
 exports.last_messages = function (req, res) {
@@ -18,12 +21,14 @@ exports.list_users = function (req, res) {
 };
 
 exports.wait_change = function (req, res) {
-
+    
 };
 
 exports.get_messages = function (req, res) {
     var id = parseInt(req.params.id);
     waiting_list[id] = waiting_list[id] || [];
     waiting_list[id].push(res);
+
+    slow_waiting_list.push(res);
 
 };
