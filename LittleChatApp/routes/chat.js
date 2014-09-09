1bac8427103e9979461f9c61908c43e6614db660
harisb <haris.botic96@gmail.com>
2014-09-09 22:42:40 +0200

    I finisihed half of the project

---
+++ LittleChatApp/routes/chat.js
@@ -0,0 +1,29 @@
+﻿/*
+ * GET users in room
+ * GET last 10 messages in room
+ */
+
+var waiting_list ={};
+
+exports.new_message = function (req, res) {
+    res.json(['nova poruka, obiscni json']);
+};
+
+exports.last_messages = function (req, res) {
+    
+};
+
+exports.list_users = function (req, res) {
+    res.json(['amerz','makunouchi','ganija']);
+};
+
+exports.wait_change = function (req, res) {
+
+};
+
+exports.get_messages = function (req, res) {
+    var id = parseInt(req.params.id);
+    waiting_list[id] = waiting_list[id] || [];
+    waiting_list[id].push(res);
+
+};
